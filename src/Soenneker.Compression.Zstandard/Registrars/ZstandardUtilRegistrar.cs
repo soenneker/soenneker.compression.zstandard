35abf318a4f198be06200bec0252c1194b9632f5
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:12 -0500

    xml commenting

--- src/Soenneker.Compression.Zstandard/Registrars/ZstandardUtilRegistrar.cs
+++ src/Soenneker.Compression.Zstandard/Registrars/ZstandardUtilRegistrar.cs
@@ -5,14 +5,27 @@ using Soenneker.Utils.File.Registrars;
 
 namespace Soenneker.Compression.Zstandard.Registrars;
 
+/// <summary>
+/// Represents the zstandard util registrar.
+/// </summary>
 public static class ZstandardUtilRegistrar
 {
+    /// <summary>
+    /// Adds zstandard util as singleton.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <returns>The result of the operation.</returns>
     public static IServiceCollection AddZstandardUtilAsSingleton(this IServiceCollection services)
     {
         services.AddFileUtilAsSingleton().TryAddSingleton<IZstandardUtil, ZstandardUtil>();
         return services;
     }
 
+    /// <summary>
+    /// Adds zstandard util as scoped.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <returns>The result of the operation.</returns>
     public static IServiceCollection AddZstandardUtilAsScoped(this IServiceCollection services)
     {
         services.AddFileUtilAsScoped().TryAddScoped<IZstandardUtil, ZstandardUtil>();
